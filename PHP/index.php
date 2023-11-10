6f87a8421f9074d11e3b70100e75d5b5630b63f8
unknown <toansd9a@gmail.com>
2023-11-10 10:13:09 +0700

    slide 24

--- PHP/index.php
+++ PHP/index.php
@@ -1,3 +1,8 @@
-<?php 
-echo"Toàn đần";
+<?php
+$TK=1000.000;
+$Pay=500.000;
+echo"số tiền ban đầu có là: $TK <br>";
+echo"số tiền cần chuyền khoản: $Pay <br>";
+$lastTK=$TK + $Pay;
+echo"số tiền sau khi chuyển khoản là: $lastTK";
 ?>
