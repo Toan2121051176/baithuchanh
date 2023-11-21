54cdee458d983f981287449aa439c1a54043fceb
unknown <toansd9a@gmail.com>
2023-11-21 18:53:49 +0700

    slide 32

--- PHP/index.php
+++ PHP/index.php
@@ -1,12 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hà Mạnh Kiên</title>
+</head>
+<body>
+    <h1> Hoán đối tiền tệ</h1>
+    <form method="post" action="index.php">
+        <label for="money">Nhập số tiền</label>
+        <input type="text"  name="money">
+        <label for="type-money"></label>
+        <input type="text" name="type-money">
+        <input type="submit" value="Hoán đối">
+    </form>
+    <?php 
+    if(isset($_POST['money'])&& isset($_POST['type-money'])) {
+        $giatien=array("USD"=>23000,"EUR"=>27000,"SGP"=>17000,"JPY"=>120);
+        $money=$_POST['money'];
+        $typemoney=strtoupper($_POST['type-money']);
+        echo"$money là bằng " . number_format($money * $giatien[$typemoney]) ."VND";
 
-<?php
-$exchangeRate = array("USD"=>22300, "EUR"=>27300, "SGD" =>17000, "JPY"=> 120,);
-
-echo "<h3>EXchange rate to VND</h3>"
-echo "USD: $exchangeRate['USD'] . <br>";
-echo "EUR: $exchangeRate['EUR'] . <br>";
-echo "SGD: $exchangeRate['SGD]  . <br>";
-echo "JPY: $exchangeRate['JPY'] . <br>";
-
-
-?>
+    }
+    ?>
+</body>
+</html>
