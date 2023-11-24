3d92144a3f2b39aeebd429d2ac692cd741a2cbd1
unknown <toansd9a@gmail.com>
2023-11-24 10:23:52 +0700

    slide 16 bài tập 3

--- PHP/php/index.php
+++ PHP/php/index.php
@@ -1,7 +1,21 @@
-<?php
-    $x = 1;
-    while ($x <= 10) {
-        echo "Day la so: $x <br>";
-      
-    }
+<?php 
+echo "<select>";
+for ($day = 1; $day <= 31; $day++) {
+    echo "<option value=\"$day\">$day</option>";
+}
+echo "</select>";
+
+echo "<select>";
+for ($month = 1; $month <= 12; $month++) {
+    echo "<option value=\"$month\">$month</option>";
+}
+echo "</select>";
+
+
+echo "<select>";
+
+for ($year = 1900; $year <= date('Y'); $year++) {
+    echo "<option value=\"$year\">$year</option>";
+}
+echo "</select>";
 ?>
