a56b4dcdcdd38d2e29250f06e98768ccaf001080
unknown <toansd9a@gmail.com>
2023-11-28 16:11:15 +0700

    slide 21

--- PHP/php/index.php
+++ PHP/php/index.php
@@ -1,14 +1,18 @@
 <?php 
-   $items = array('item1.jpg', 'item2.jpg', 'item3.jpg', 'item4.jpg');
-   foreach($items as $item)
+   function dislay_image_list($imagelisst)
    {
-    echo "<img src='images/$item' width = 50px height =50px alt='$item'>";
+    foreach ($imageList as $item)
+    {
+        echo "<img src='images/$item' width = 50px height =50px alt='$item'>";
+ 
+    }
    }
+   $items = array('item1.jpg', 'item2.jpg', 'item3.jpg', 'item4.jpg');
+   dislay_image_list($items);
+   
 ?>
 <h1>function</h1>
 <?php
      $items = array('item5.png', 'item6.png');
-     foreach ($items as $item)
-     {
-        echo "<img src='images/$item' width = 50px height =50px alt='$item'> ";
-     }
+     dislay_image_list($items);
+?>
