156b0dc9b92b8bad1d623b95537ec9b20b0d6f90
unknown <toansd9a@gmail.com>
2023-11-28 15:57:35 +0700

    slide 20

--- PHP/php/index.php
+++ PHP/php/index.php
@@ -1,7 +1,14 @@
 <?php 
-   $items = array('1.jpg', '2.jpg', '3.jpg', '4.jpg');
+   $items = array('item1.jpg', 'item2.jpg', 'item3.jpg', 'item4.jpg');
    foreach($items as $item)
    {
-    echo "<img src='images/$item' width = 50px height =50px alt='$item'>"
+    echo "<img src='images/$item' width = 50px height =50px alt='$item'>";
    }
 ?>
+<h1>function</h1>
+<?php
+     $items = array('item5.png', 'item6.png');
+     foreach ($items as $item)
+     {
+        echo "<img src='images/$item' width = 50px height =50px alt='$item'> ";
+     }
