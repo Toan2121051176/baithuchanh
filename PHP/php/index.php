fcc92d5e16b3922047e5317a4e18390326a119e2
unknown <toansd9a@gmail.com>
2023-12-04 21:38:53 +0700

    slide 55

--- PHP/php/index.php
+++ PHP/php/index.php
@@ -1,44 +1,41 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<form method="post" action="index.php">
-<label>First Number</label><br>
-<input type="text" name="first"><br>
-<label>Second Number</label><br>
-<input type="text" name="second"><br>
-<input type="submit" value="+"name="+">
-<input type="submit" value="-"name="-">
-<input type="submit" value="x"name="x">
-<input type="submit" value="/"name="/">
-</form>
-<?php 
-if(isset($_POST['+'])){
-$kq=$_POST['first']+$_POST['second'];
-echo $kq;
-
-}
-if(isset($_POST['-'])){
-    $kq=$_POST['first']+$_POST['second'];
-    echo $kq;
-
+<?php
+require('connect.php');
+mysqli_set_charset($conn,'UTF8');
+$sql="select * from products";
+$ketqua=$conn->query($sql);
+if($ketqua->num_rows > 0) {
+    echo"<table>
+    <tr>
+    <th>mã sản phẩm</th>
+    <th>tên sản phẩm</th>
+    <th>số lượng </th>
+    <th>giá </th>
+    </tr>"
+    ;
+    while($row = $ketqua->fetch_assoc()) {
+        echo"<tr>
+        <td>".$row["id"]."</td>
+        <td>".$row["name"]."</td>
+        <td>".$row["quality"]."</td>
+        <td>".$row["price"]."</td>
+        </tr>";
     }
-if(isset($_POST['x'])){
-    $kq=$_POST['first']+$_POST['second'];
-    echo $kq;
-
-        }
-if(isset($_POST['/'])){
-    $kq=$_POST['first']+$_POST['second'];
-    echo $kq;
-
+    echo "<style>
+    table{
+        width:100%;
+        boder-collapse:collapse;
+    }
+    tr,th,td{
+        border:1px solid black;
+        boder-collapse:collapse;
+        text-align:center;
+    }
+    </style>";
 }
-
+else {
+    echo"không có dữ liệu";
+}
+$ketqua->close();
 ?>
-</body>
-</html>
+
