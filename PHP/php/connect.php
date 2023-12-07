c98f657b0aff4c4e3604b7a803733931cfad096f
unknown <toansd9a@gmail.com>
2023-12-07 21:38:07 +0700

    slide 64

---
+++ PHP/php/connect.php
@@ -0,0 +1,13 @@
+<?php 
+$servername = "localhost";
+$username='root';
+$password ="";
+$db="oderfood";
+$conn = new mysqli($servername,$username,$password,$db);
+if($conn->connect_error){
+    echo"kết nối bị lỗi",$conn->connect_error;
+}
+else{
+    echo("Thành công");
+}
+?>
